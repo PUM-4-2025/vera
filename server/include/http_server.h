8d34d795f116daa1e7f5b1182713ec59f2aadc9b
an0nymoos3 <moosely123@gmail.com>
2025-04-02 10:42:40 +0200

    initial webserver written in c++ mongoose, linker issues in server

---
+++ server/include/http_server.h
@@ -0,0 +1,36 @@
+/*
+ * Abstraction class for building a webserver and handling web requests.
+ */
+
+#ifndef HTTPSERVER_H
+#define HTTPSERVER_H
+
+#include "../extern/mongoose/mongoose.h"
+
+#include <string>
+
+class HttpServer {
+public:
+  /**
+   * Pass adress as a string to listen to, e.g. "localhost:8080"
+   */
+  HttpServer();
+  ~HttpServer();
+
+  void listenTo(std::string adress);
+  void start();
+
+  // TODO: Come back and iterate on function below. This is meant
+  // to be a function to allow other modules, like the audio
+  // analysis to register a handler for a specific api path,
+  // e.g. ("/api/audio", audio_analysis_func).
+  void registerHandler(std::string api_path, void (*f)(void));
+
+private:
+  struct mg_mgr m_mgr;
+  std::string m_adress;
+
+  static void eventHandler(struct mg_connection *c, int ev, void *ev_data);
+};
+
+#endif
---
+++ server/src/http_server/http_server.cpp
@@ -0,0 +1,18 @@
+#include "../../include/http_server.h"
+
+void HttpServer::listenTo(std::string adress) {
+  m_adress = adress;
+}
+
+void HttpServer::start() {
+  mg_mgr_init(&m_mgr);
+  mg_http_listen(&m_mgr, m_adress.c_str(), eventHandler, nullptr);
+
+  for (;;) {
+    // Using 1000 from mongooses own example:
+    // https://mongoose.ws/documentation/#2-minute-integration-guide
+    mg_mgr_poll(&m_mgr, 1000);
+  }
+}
+
+void HttpServer::eventHandler(struct mg_connection *c, int ev, void *ev_data) {}
