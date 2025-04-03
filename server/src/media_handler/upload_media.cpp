837005ba0c95e19b2e295f8ffd7233c83ad42436
an0nymoos3 <moosely123@gmail.com>
2025-04-03 15:01:06 +0200

    server can recieve and send https json requests

--- server/src/media_handler/upload_media.cpp
+++ server/src/media_handler/upload_media.cpp
@@ -1,5 +1,17 @@
 #include "upload_media.h"
 
+#include "../../extern/json/json.hpp"
+using json = nlohmann::json;
+
+/**
+ * Returns a unique id for uploading media.
+ * TODO: Replace with proper implementation once sessions are
+ * able to be tracked.
+ */
+int get_upload_id() {
+  return 1;
+}
+
 void register_media_handlers(HttpServer &server) {
   server.registerHandler("/api/v1/uploads/initiate", init_upload);
   server.registerHandler("/api/v1/uploads/chunks", upload_chunk);
@@ -7,9 +19,16 @@ void register_media_handlers(HttpServer &server) {
 }
 
 void init_upload(struct mg_connection *c, struct mg_http_message *msg) {
-  mg_str body = msg->body;
+  std::string body = msg->body.buf;
+  json json_body = json::parse(body);
 
-  mg_http_reply(c, 200, "Content-Type: text/plain\r\n", body.buf);
+  int upload_id = get_upload_id();
+  json response = {{"uploadId", upload_id},
+                   {"status", "initiated"},
+                   {"message", "Upload session initiated successfully."}};
+  std::string response_str = response.dump();
+
+  mg_http_reply(c, 200, "Content-Type: application/json\r\n", response_str.c_str());
 }
 
 void upload_chunk(struct mg_connection *, struct mg_http_message *) {}
