868afba91b0425d7ec21063365ed9fc647c29d09
an0nymoos3 <moosely123@gmail.com>
2025-04-03 18:38:52 +0200

    working on handling general uploads

--- server/include/upload_media.h
+++ server/include/upload_media.h
@@ -3,12 +3,33 @@
 
 #include "http_server.h"
 
+#include <mutex>
+#include <vector>
+
 void register_media_handlers(HttpServer &server);
+void init_upload(struct mg_connection *c, struct mg_http_message *msg);
+void upload_status(struct mg_connection *c, struct mg_http_message *msg);
+void upload_chunk(struct mg_connection *c, struct mg_http_message *msg);
 
-void init_upload(struct mg_connection *, struct mg_http_message *);
+typedef struct UploadSession {
+  std::string filename;
+  int file_size;
+  int session_id;
+  int completed_chunks;
+  int total_chunks;
+} UploadSession;
 
-void upload_status(struct mg_connection *, struct mg_http_message *);
+class UploadHandler {
+public:
+  UploadHandler();
+  ~UploadHandler();
 
-void upload_chunk(struct mg_connection *, struct mg_http_message *);
+  void new_session(UploadSession session);
+  void remove_session(int upload_id);
+
+private:
+  std::vector<UploadSession> m_uploads_;
+  std::mutex m_uploads_guard_;
+};
 
 #endif
--- server/src/media_handler/upload_media.cpp
+++ server/src/media_handler/upload_media.cpp
@@ -3,6 +3,8 @@
 #include "../../extern/json/json.hpp"
 using json = nlohmann::json;
 
+UploadHandler handler;
+
 /**
  * Returns a unique id for uploading media.
  * TODO: Replace with proper implementation once sessions are
@@ -21,8 +23,26 @@ void register_media_handlers(HttpServer &server) {
 void init_upload(struct mg_connection *c, struct mg_http_message *msg) {
   std::string body = msg->body.buf;
   json json_body = json::parse(body);
+  std::string file_name = json_body["fileName"];
+  int file_size = json_body["fileSize"];
 
   int upload_id = get_upload_id();
+  int num_chunks = 0;
+  int total_chunks = file_size / 5242880;
+
+  std::string filename;
+  int file_size;
+  int session_id;
+  int completed_chunks;
+  int total_chunks;
+
+  UploadSession new_session = {
+      upload_id,
+      0,
+
+  };
+  handler.new_session(new_session);
+
   json response = {{"uploadId", upload_id},
                    {"status", "initiated"},
                    {"message", "Upload session initiated successfully."}};
@@ -34,3 +54,16 @@ void init_upload(struct mg_connection *c, struct mg_http_message *msg) {
 void upload_chunk(struct mg_connection *, struct mg_http_message *) {}
 
 void upload_status(struct mg_connection *, struct mg_http_message *) {}
+
+UploadHandler::UploadHandler() {};
+UploadHandler::~UploadHandler() {
+  m_uploads_guard_.lock();
+  m_uploads_.clear();
+  m_uploads_guard_.unlock();
+}
+
+void UploadHandler::new_session(UploadSession session) {
+  m_uploads_guard_.lock();
+  m_uploads_.push_back(session);
+  m_uploads_guard_.unlock();
+}
