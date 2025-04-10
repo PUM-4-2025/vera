bfca21308309c937aa20856abb539faa36c031c1
an0nymoos3 <70707989+an0nymoos3@users.noreply.github.com>
2025-04-10 14:43:24 +0200

    Backend functionality for uploading media from client to server (#18)
    * server can accept and return basic http requests
    
    * added json.hpp download for server json handling
    
    * server can recieve and send https json requests
    
    * working on handling general uploads
    
    * updated setup.py to include submodules
    
    * removed outdated comment
    
    * Revert "uppdaterat README (#11)"
    
    This reverts commit de56bf0d6596e5fce65eac4bf16171788dbf1ed4.
    
    * finished init_upload
    
    * ingore .clangd stuff
    
    * added random ids
    
    * fixed issues from merging with dev
    
    * added check for uniqueness
    
    * reorganized C++ code for clearer code structure
    
    * added missing return statement, fixed some incorrect formatting and shit
    
    * made completed_chunks atomic to make sure the reading is correct when checking that upload is done later
    
    * added basic UserSession for implementing segregation of file uploads
    
    * upgraded to C++ 17 for filesystem support
    
    * creates new directory in /tmp for session
    
    * can now upload text over chunked api requests
    
    * added support for uploading chunked files
    
    * fixing assemle file issues
    
    * fixed memory related issue of removing session from handler too early. chunks can now be assembled
    
    * server side can accept base64 encoded strings to represent binary data and write to fiels
    
    * extraced some common behaviour into helper function
    
    * fixed issues before merging

---
+++ server/src/media_handler/upload_handler.cpp
@@ -0,0 +1,75 @@
+#include "upload_handler.h"
+#include "upload_media.h"
+#include <vector>
+
+/*
+ * A lot of methods are written in here to stop race conditions
+ * from happening. Even though some methods would be trivial to 
+ * write in ohter functions, the locking mechanism is needed
+ * to assure that uploads are synced. 
+ */
+
+UploadHandler::UploadHandler() {};
+UploadHandler::~UploadHandler() {
+  m_uploads_guard_.lock();
+  m_uploads_.clear();
+  m_uploads_guard_.unlock();
+}
+
+void UploadHandler::newSession(const UploadSession &session) {
+  m_uploads_guard_.lock();
+  m_uploads_.push_back(session);
+  m_uploads_guard_.unlock();
+}
+
+void UploadHandler::removeSession(const UploadSession &session) {
+  m_uploads_guard_.lock();
+  for (auto it = m_uploads_.begin(); it != m_uploads_.end();) {
+    if (it->session_id == session.session_id) {
+      m_uploads_.erase(it);
+      break;
+    }
+  }
+  m_uploads_guard_.unlock();
+}
+
+bool UploadHandler::isUniqueId(int id) {
+  m_uploads_guard_.lock();
+  
+  for (const auto &session : m_uploads_) {
+    if (session.session_id == id) {
+      m_uploads_guard_.unlock();
+      return false;
+    }
+  }
+
+  m_uploads_guard_.unlock();
+  return true;
+}
+
+UploadSession* UploadHandler::getSession(int id) {
+  m_uploads_guard_.lock();
+  
+  for (auto &session : m_uploads_) {
+    if (session.session_id == id) {
+      m_uploads_guard_.unlock();
+      return &session;
+    }
+  }
+
+  m_uploads_guard_.unlock();
+  return nullptr;
+}
+
+void UploadHandler::incrementChunk(UploadSession &session) {
+  m_uploads_guard_.lock();
+  session.completed_chunks++;
+  m_uploads_guard_.unlock();
+}
+
+bool UploadHandler::sessionCompleted(const UploadSession &session) {
+  m_uploads_guard_.lock();
+  bool is_complete = session.completed_chunks == session.total_chunks;
+  m_uploads_guard_.unlock();
+  return is_complete;
+}
