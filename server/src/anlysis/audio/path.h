82c65bdfa982062b5788b9e826e4691dcbaae93b
Oscar Gustafsson <oscgu942@student.liu.se>
2025-04-11 13:55:54 +0200

    create cpp files for audio extraction

---
+++ server/src/anlysis/audio/path.h
@@ -0,0 +1,6 @@
+#ifndef PATH_H
+#define PATH_H
+
+std::string PATH = "C:\\Users\\oscar\\Desktop\\PUM4\\ljud\\";
+
+#endif
--- server/src/anlysis/audio/run.cpp
+++ server/src/anlysis/audio/run.cpp
@@ -1,10 +1,11 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include "run.h"
 using namespace std;
 
-int main(const char* in_video, int threshold_db, int min_sec){
+void run(string in_video, int threshold_db, int min_sec){
     string prefix = "py run.py ";
     string run = prefix + in_video + " " + to_string(threshold_db) + " " + to_string(min_sec);
-    std::system(run.c_str());
+    system(run.c_str());
 }
