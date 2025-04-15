85584491e7245d2dbd3e4e63dbebf5647084058d
Oscar Gustafsson <oscgu942@student.liu.se>
2025-04-15 10:40:28 +0200

    change how amplitude flotes are stored

--- server/src/anlysis/audio/visual.py
+++ server/src/anlysis/audio/visual.py
@@ -9,9 +9,9 @@ PATH = path().PATH
 
 # === Konfiguration ===
 WINDOW_DURATION = 0.01  # sekunder
-EPSILON = 1e-10
+EPSILON = 1e-4
 MAX_AMPLITUDE = 32767  # För 16-bit PCM
-DB_REF = 30
+DB_REF = 0
 
 def wav_to_db_csv(in_wav):
     with wave.open(PATH + in_wav, 'rb') as wav_file:
@@ -27,7 +27,7 @@ def wav_to_db_csv(in_wav):
     window_size = int(sample_rate * WINDOW_DURATION)
     num_windows = len(raw_signal) // window_size
 
-    time_axis = []
+    #time_axis = [] always the same amount of samples per second (100 Hz)
     db_values = []
 
     for i in range(num_windows):
@@ -38,19 +38,26 @@ def wav_to_db_csv(in_wav):
         amplitude = np.max(np.abs(window)) / MAX_AMPLITUDE
         amplitude = max(amplitude, EPSILON)
         db = 20 * np.log10(amplitude) + DB_REF
-        t = round(i * WINDOW_DURATION, 4)
+        #t = round(i * WINDOW_DURATION, 4)
 
-        db_values.append(db if amplitude >= 1e-4 else np.nan)
-        time_axis.append(t)
+        db_values.append(db)
+        #time_axis.append(t)
 
     # Skriv till CSV
-    with open(PATH + in_wav + ".csv", 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Time (s)', 'Amplitude (dB)'])
-        for t, db in zip(time_axis, db_values):
-            writer.writerow([t, db])
+    with open(PATH + in_wav + ".txt", 'w', newline='') as txt:
+        #writer = csv.writer(csvfile)
+        print(len(db_values))
+        for i in range(len(db_values)):
+            if len(db_values) == i:
+                txt.write(round(db_values[i], 1))
+            else:
+                txt.write("{}, ".format(round(db_values[i], 1)))
+        #writer.writerow(['Time (s)', 'Amplitude (dB)'])
+        #for t, db in zip(time_axis, db_values):
+        #for db in  db_values:
+        #    writer.writerow([t, db])
 
-    sys.stdout.write(f"Saved dB data to: {PATH + in_wav + ".csv"}")
+    sys.stdout.write(f"Saved dB data to: {PATH + in_wav + ".txt"}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -59,8 +66,8 @@ if __name__ == "__main__":
 
     input_wav = sys.argv[1]
 
-    if not os.path.exists(input_wav):
-        print(f"File not found: {input_wav}")
+    if not os.path.exists(PATH + input_wav):
+        print(f"File not found: {PATH + input_wav}")
         sys.exit(1)
 
     wav_to_db_csv(input_wav)
