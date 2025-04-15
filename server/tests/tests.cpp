80365186322dcc3ee09c71452c6d669cafe82e58
Edvin Sätterlund <edvinsatterlund@edvins-air.students.netlogon.liu.se>
2025-04-15 13:37:54 +0200

    Test som ska passa

--- server/tests/tests.cpp
+++ server/tests/tests.cpp
@@ -30,7 +30,7 @@ TEST_CASE("extract creates a .wav file from video input", "[extract]") {
     cout << "TEEEEESSSSSSTTTTTAAAAAAARRRRRR-------" << endl;
  
     // Verifiera att .wav-filen nu finns
-    REQUIRE(fs::exists(wav_file));
+    REQUIRE(fs::exists(PATH + wav_file));
 
     // Städa upp
     fs::remove(wav_file);
