37c364f6f3953c4ee56e435f2a5e2f3f61531073
an0nymoos3 <moosely123@gmail.com>
2025-04-24 20:05:51 +0200

    http_utils.h

---
+++ server/include/http_utils.h
@@ -0,0 +1,11 @@
+#ifndef HTTP_UTILS_H
+#define HTTP_UTILS_H
+
+#include <string>
+#include "mongoose.h"
+
+void send_http_response(struct mg_connection *c, int http_code, int id, std::string status,std::string message);
+
+int handlePreflight(struct mg_connection *c, struct mg_http_message *msg);
+
+#endif
