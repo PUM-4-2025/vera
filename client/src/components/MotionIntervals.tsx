c296a67bc28cea825f692b9027620997284ec447
an0nymoos3 <moosely123@gmail.com>
2025-05-23 00:58:53 +0200

    fixed client compilation

--- client/src/components/MotionIntervals.tsx
+++ client/src/components/MotionIntervals.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useAnalysis } from '@/contexts/AnalysisContext';
 import { useProject } from '@/contexts/ProjectContext';
 
