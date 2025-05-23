15f254155b46eab198cfde4e8d93b0afa8302476
Edvin Sätterlund <edvinsatterlund@c-10-254-13-7.clients.eduroam.liu.se>
2025-05-23 17:20:48 +0200

    Implemented MOTION_THRESHOLD depending on roi size instead of fixed pixels

--- server/src/anlysis/video/motiondetection.cpp
+++ server/src/anlysis/video/motiondetection.cpp
@@ -1,11 +1,18 @@
+
 #include "video.h"
-#include <vector>
-#include <opencv2/opencv.hpp>
+
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+// Tid som det ska vara still för att avsluta motion
 #define STOP_IF_STILL_FOR 0.5
 
-std::vector<int> analyse_video(std::vector<int> cords){
-    cv::VideoCapture cap("robber.mp4");
+// Andel pixlar ändrade för motion
+#define MOTION_THRESHOLD 0.001
+
+std::vector<int> analyse_video(std::string path, std::vector<int> cords) {
+    cv::VideoCapture cap(path);
     int video_width = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_WIDTH));
     int video_height = static_cast<int>(cap.get(cv::CAP_PROP_FRAME_HEIGHT));
 
@@ -17,15 +24,15 @@ std::vector<int> analyse_video(std::vector<int> cords){
     bool in_motion = false;
     int motion_start = 0;
     int motion_end = 0;
-    int no_motion_count = 0;  
-    std::vector<int> motion_frames; 
+    int no_motion_count = 0;
+    std::vector<int> motion_frames;
 
     double fps = cap.get(cv::CAP_PROP_FPS);
-    int stop_still_for_frames = static_cast<int>(std::round(STOP_IF_STILL_FOR*fps));
+    int stop_still_for_frames = static_cast<int>(std::round(STOP_IF_STILL_FOR * fps));
 
     if (!cap.isOpened()) {
         std::cerr << "Error: Cannot open video file.-------------------------" << std::endl;
-        return {};
+        throw std::invalid_argument("Invalid filepath");
     }
 
     if (w <= 0 || h <= 0) {
@@ -36,19 +43,14 @@ std::vector<int> analyse_video(std::vector<int> cords){
         throw std::invalid_argument("ROI is outside of video bounds.");
     }
 
-    if (!cap.isOpened()) {
-        std::cerr << "Error: Cannot open video file.-------------------------" << std::endl;
-        return motion_frames; 
-    }
-
     cv::Ptr<cv::BackgroundSubtractor> subtractor = cv::createBackgroundSubtractorMOG2();
     cv::Mat frame, fgMask, cropped_frame;
     int frame_counter = 0;
-    
-    while(true){
+
+    while (true) {
         cap >> frame;
         if (frame.empty())
-            break;
+        break;
 
         cv::Rect roi(x, y, w, h);
         cv::Mat cropped_frame = frame(roi);
@@ -58,38 +60,39 @@ std::vector<int> analyse_video(std::vector<int> cords){
         cv::erode(fgMask, fgMask, cv::Mat());
         cv::dilate(fgMask, fgMask, cv::Mat());
 
-        double motion_pixels = cv::countNonZero(fgMask);
+        unsigned int motion_pixels = cv::countNonZero(fgMask);
 
-        if (motion_pixels > 500) { // Finjustera
-            if (!in_motion) {
-                // Starta rörelse
-                motion_start = frame_counter;
-                in_motion = true;
-                no_motion_count = 0;
-            }
-            // Rörelse, så vi nollställer räknaren
-            no_motion_count = 0;
+        // Hoppa över första då denna alltid har 100% pixelförändring
+        if (frame_counter == 0){
+        frame_counter += 1;
+        continue;
+        }
+
+        if (motion_pixels > w * h * MOTION_THRESHOLD) {  // Finjustera
+        if (!in_motion) {
+            // Starta rörelse
+            motion_start = frame_counter;
+            in_motion = true;
+        }
+        // Rörelse, så vi nollställer räknaren
+        no_motion_count = 0;
         } else {
-            no_motion_count++;
-            // Stanna rörelse och lägg till start & slut
-            if (in_motion && no_motion_count >= stop_still_for_frames) {
-                motion_end = frame_counter - 1;
-                motion_frames.push_back(motion_start);
-                motion_frames.push_back(motion_end);
-                in_motion = false;
-                no_motion_count = 0;
-            }
+        no_motion_count++;
+        // Stanna rörelse och lägg till start & slut
+        if (in_motion && no_motion_count >= stop_still_for_frames) {
+            motion_end = frame_counter - 1;
+            motion_frames.push_back(motion_start);
+            motion_frames.push_back(motion_end);
+            in_motion = false;
+            no_motion_count = 0;
+        }
         }
         frame_counter += 1;
     }
 
-    // Skriver ut alla rörelse-bilder (start & slut)
-    for( int i : motion_frames){
-        std::cout << i/fps << std::endl;
-    }
-
     cap.release();
     cv::destroyAllWindows();
-    
+
     return motion_frames;
 }
+
